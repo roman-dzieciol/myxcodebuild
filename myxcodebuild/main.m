e38d45ccf3bbc7cb4f13e8c505f545d2597ca804
Roman Dzięcioł <roman.dzieciol@gmail.com>
2019-03-10 05:46:32 +0000

    Add main method implementation

--- myxcodebuild/main.m
+++ myxcodebuild/main.m
@@ -1,18 +1,31 @@
-//
-//  main.m
-//  myxcodebuild
-//
-//  Created by Roman Dzieciol on 3/10/19.
-//  Copyright © 2019 Roman Dzieciol. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import "Xcode3CommandLineBuildTool.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+
+        // Initialize the Xcode object that does actual work
+        Xcode3CommandLineBuildTool *buildTool = [Xcode3CommandLineBuildTool sharedCommandLineBuildTool];
+
+        // Set name to be used in output
+        buildTool.name = [[processInfo.arguments firstObject] lastPathComponent];
+
+        // Pass all commandline arguments, except for the path to binary
+        buildTool.arguments = [processInfo.arguments subarrayWithRange:NSMakeRange(1, processInfo.arguments.count-1)];
+
+        // Use the same environment variables as the myxcodebuild binary
+        buildTool.environment = processInfo.environment;
+
+        // Use standard output & error for logging, ignore standard input
+        buildTool.standardError = [NSFileHandle fileHandleWithStandardError];
+        buildTool.standardOutput = [NSFileHandle fileHandleWithStandardOutput];
+        buildTool.standardInput = [NSFileHandle fileHandleWithNullDevice];
+
+        // Run the command
+        [buildTool run];
+
+        // Return exit code
+        return (int)buildTool.exitStatus;
     }
-    return 0;
 }
