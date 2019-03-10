4a398689da020f61cbb628412bec599d78efefbc
Roman Dzięcioł <roman.dzieciol@gmail.com>
2019-03-10 05:41:11 +0000

    Initial Commit

---
+++ myxcodebuild/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  myxcodebuild
+//
+//  Created by Roman Dzieciol on 3/10/19.
+//  Copyright © 2019 Roman Dzieciol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
